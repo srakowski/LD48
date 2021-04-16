fec0c91067895a289e91abfdaf67374b27a3dbc3
Shawn Rakowski <srakowski@live.com>
2021-04-15 21:00:56 -0500

    start on engine/editor

---
+++ LD48.Editor/SceneViewer.cs
@@ -0,0 +1,25 @@
+﻿namespace LD48.Editor
+{
+    using Microsoft.Xna.Framework;
+    using System;
+    using System.Collections.Generic;
+    using System.Text;
+
+    class SceneViewer : Game
+    {
+        public int b;
+
+        private GraphicsDeviceManager _graphics;
+        public SceneViewer()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            IsMouseVisible = true;
+            Window.AllowUserResizing = true;
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(new Color(255, 255, b, 255));
+        }
+    }
+}
